5133bac3a5b9c60e0cec2bd74bfcedebdcee6f4a
TibeDeClercq <79404302+TibeDeClercq@users.noreply.github.com>
2022-02-16 11:54:01 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var cors = require('cors');
 app.use(
     cors({
         credentials: true,
-        origin: 'https://nostalgic-newton-aeb45c.netlify.app/'
+        origin: 'https://nostalgic-newton-aeb45c.netlify.app'
     })
 );
 app.options('*', cors());
